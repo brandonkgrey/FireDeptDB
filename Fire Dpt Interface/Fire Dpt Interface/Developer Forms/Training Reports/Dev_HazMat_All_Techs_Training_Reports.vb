cde74de2cff9c80438db2ccd53928fb4694b4338
alejandrovega44 <alejandrovega44@gmail.com>
2015-03-30 20:20:00 -0500

    Revert "Transferred Exiting Database Forms"
    This reverts commit 5cbba718f253c70069bfa65aa42e55e55426520e.

--- Fire Dpt Interface/Fire Dpt Interface/Developer Forms/Training Reports/Dev_HazMat_All_Techs_Training_Reports.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Dev_HazMat_All_Techs_Training_Reports
-    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
-        Dim DevForm As Developer_Training_Reports
-        DevForm = New Developer_Training_Reports()
-        DevForm.Show()
-        DevForm = Nothing
-        Me.Close()
-    End Sub
-End Class
--- Fire Dpt Interface/Fire Dpt Interface/Supervisor Forms/SupervisorForm.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class SupervisorForm
-
-End Class
